3ec4f26b119269e9dd213f85ab02f17a7b3902f6
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2022-10-30 18:04:33 +0700

    added comments

--- student.h
+++ student.h
@@ -1,17 +1,26 @@
 #pragma once
+
 #include <iostream>
 
 struct student {
+
+    //assigns the value 0 to age_ and assigns the value '\0' to name_
     student();
 
+    //assigns value of age to  age_ and assigns value of name to name_
     student(unsigned age, std::string name);
 
+    //returns true if fields age_ and name_ are equal
+    //returns false if fields age_ or name_ are not equal
     bool operator==(const student &other) const;
 
+    //returns true if fields age_ or name_ are equal
+    //returns false if fields age_ and name_ are not equal
     bool operator!=(const student &other) const;
 
-    long long int hash() const; //counts hash for the student
+    //counts hash for the student
     //returns value of hash
+    long long int hash() const;
 
     unsigned age_;
     std::string name_;
