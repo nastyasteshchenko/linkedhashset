e4ca91d5945293e94ead48288dc1e20df00d9d74
nastyasteshchenko <a.steshchenko@g.nsu.ru>
2022-11-08 13:19:46 +0700

    changed comments on functions

--- student.h
+++ student.h
@@ -7,15 +7,12 @@ struct student {
     //assigns the value 0 to age_ and assigns the value '\0' to name_
     student();
 
-    //assigns value of age to  age_ and assigns value of name to name_
     student(unsigned age, std::string name);
 
-    //returns true if fields age_ and name_ are equal
-    //returns false if fields age_ or name_ are not equal
+    //returns true if ages and names equal
+    //returns false if ages or names are not equal
     bool operator==(const student &other) const;
 
-    //returns true if fields age_ or name_ are equal
-    //returns false if fields age_ and name_ are not equal
     bool operator!=(const student &other) const;
 
     //counts hash for the student
